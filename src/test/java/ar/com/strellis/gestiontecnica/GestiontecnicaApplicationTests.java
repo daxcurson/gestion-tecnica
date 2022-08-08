ab6b1ebeb7447313cfbc2dcc0b43f88e62f06174
Agustín Villafañe <daxcurson@users.noreply.github.com>
2022-08-08 20:30:15 -0300

    Gestion tecnica!
    Primer commit de un backend REST para una aplicacion de servicio tecnico

---
+++ src/test/java/ar/com/strellis/gestiontecnica/GestiontecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.strellis.gestiontecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiontecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
